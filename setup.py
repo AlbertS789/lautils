84f8f6ed133faddccb975271117ee6527954bb68
Al <AlbertS789@users.noreply.github.com>
2023-08-05 16:45:51 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='lautils',
+      version='1.0',
+      description='Utilities',
+      author='L.A',
+      author_email='tbd@tbd.com',
+      url='https://github.com/AlbertS789/lautils',
+      packages=['lautils'],
+     )
